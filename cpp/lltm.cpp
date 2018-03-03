3dbe2719b2bdc4f63e0f9c025c997f648a8110c1
Peter Goldsborough <psag@fb.com>
2018-03-02 17:26:13 -0800

    Added grad_check.py

--- cpp/lltm.cpp
+++ cpp/lltm.cpp
@@ -28,19 +28,23 @@ std::vector<at::Tensor> lltm_forward(
     at::Tensor old_cell) {
   auto X = at::cat({old_h, input}, /*dim=*/1);
 
-  auto gates = at::addmm(bias, X, weights.transpose(0, 1));
+  auto gate_weights = at::addmm(bias, X, weights.transpose(0, 1));
+  auto gates = gate_weights.chunk(3, /*dim=*/1);
 
-  const auto state_size = old_h.size(1);
-  auto input_gate = at::sigmoid(gates.slice(/*dim=*/1, 0, state_size));
-  auto output_gate =
-      at::sigmoid(gates.slice(/*dim=*/1, state_size, 2 * state_size));
-  auto candidate_cell =
-      at::elu(gates.slice(/*dim=*/1, 2 * state_size), /*alpha=*/1.0);
+  auto input_gate = at::sigmoid(gates[0]);
+  auto output_gate = at::sigmoid(gates[1]);
+  auto candidate_cell = at::elu(gates[2], /*alpha=*/1.0);
 
   auto new_cell = old_cell + candidate_cell * input_gate;
   auto new_h = at::tanh(new_cell) * output_gate;
 
-  return {new_h, new_cell, input_gate, output_gate, candidate_cell, X, gates};
+  return {new_h,
+          new_cell,
+          input_gate,
+          output_gate,
+          candidate_cell,
+          X,
+          gate_weights};
 }
 
 std::vector<at::Tensor> lltm_backward(
@@ -51,33 +55,28 @@ std::vector<at::Tensor> lltm_backward(
     at::Tensor output_gate,
     at::Tensor candidate_cell,
     at::Tensor X,
-    at::Tensor gates,
-    at::Tensor weights,
-    at::Tensor old_cell) {
-      auto d_gates = at::zeros_like(gates);
-      const auto state_size = grad_h.size(1);
-
-  // d_gates.slice(1, state_size, 2 * state_size) = at::tanh(new_cell) * grad_h;
+    at::Tensor gate_weights,
+    at::Tensor weights) {
+  auto d_output_gate = at::tanh(new_cell) * grad_h;
   auto d_tanh_new_cell = output_gate * grad_h;
   auto d_new_cell = d_tanh(new_cell) * d_tanh_new_cell + grad_cell;
 
-
   auto d_old_cell = d_new_cell;
-  // d_gates.slice(1, 2 * state_size) = input_gate * d_new_cell;
-  // d_gates.slice(1, 0, state_size) = candidate_cell * d_new_cell;
-  //
-  // d_gates.slice(1, 0, 2 * state_size) *= d_sigmoid(gates.slice(/*dim=*/1, 0, 2 * state_size));
-  // // d_output_gate *=
-  // //     d_sigmoid(gates.slice(/*dim=*/1, state_size, 2 * state_size));
-  // d_gates.slice(1, 2 * state_size) *= d_elu(gates.slice(/*dim=*/1, 2 * state_size));
+  auto d_candidate_cell = input_gate * d_new_cell;
+  auto d_input_gate = candidate_cell * d_new_cell;
 
-  // auto d_gates =
-  //     at::cat({d_input_gate, d_output_gate, d_candidate_cell}, /*dim=*/1);
+  auto gates = gate_weights.chunk(3, /*dim=*/1);
+  d_input_gate *= d_sigmoid(gates[0]);
+  d_output_gate *= d_sigmoid(gates[1]);
+  d_candidate_cell *= d_elu(gates[2]);
+
+  auto d_gates =
+      at::cat({d_input_gate, d_output_gate, d_candidate_cell}, /*dim=*/1);
 
   auto d_weights = d_gates.t().mm(X);
   auto d_bias = d_gates.sum(/*dim=*/0, /*keepdim=*/true);
 
-  auto d_X = d_gates.mm(weights).split(state_size, /*dim=*/1);
+  auto d_X = d_gates.mm(weights).split(new_cell.size(1), /*dim=*/1);
 
   return {d_X[0], d_X[1], d_weights, d_bias, d_old_cell};
 }
